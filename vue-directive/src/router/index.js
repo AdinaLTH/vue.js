2f34370e132c0ab490a7d908636e0d3cf24503a4
taehotaeho <jackt0506@naver.com>
2026-03-03 11:06:01 +0900

    0303 2교시

--- vue-directive/src/router/index.js
+++ vue-directive/src/router/index.js
@@ -1,23 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
+  // history 객체와 연동여부
   history: createWebHistory(import.meta.env.BASE_URL),
+  // vue-router가 사용하는 route(endpoint + service + view or data)정보
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/", // 라우트경로(필수)
+      name: "home", // 라우트이름(선택이지만 사용권장)
+      // component: HomeView, 라우트를 통해서 불러낼 컴포넌트
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/AboutView.vue"), // 동적임포트: 실질적으로 필요할 때 당겨옴
     },
   ],
-})
+});
 
-export default router
+export default router;
---
+++ vue-directive/src/views/DirectiveBind.vue
@@ -0,0 +1,25 @@
+<script setup>
+// composition APi
+import { ref, reactive, computed } from "vue";
+const info = ref("<strong>https://vuejs.orj</strong>");
+const url = ref("https://vuejs.orj");
+const isDisabled = ref(true);
+const imgAttributes = reactive(
+  // 필드명 => 속성이름
+  { src: "./src/assets/logo.svg", alt: "vue 기본로고", width: "150px" },
+);
+</script>
+<template>
+  <h2>특정 속성의 경우</h2>
+  <p v-html="info"></p>
+  <p v-text="info"></p>
+  <p>vuejs 공식 사이트: {{ info }}</p>
+  <hr />
+  <h2>일반속성</h2>
+  <p><a href="https://vuejs.orj">vue.js사이트</a></p>
+  <p><a v-bind:href="url">vue.js사이트</a></p>
+  <p><a v-bind:href="url + '/guide/introduction.html'">vue.js사이트</a></p>
+  <p><button v-bind:disabled="isDisabled">동의합니다</button></p>
+  <p><img alt="vue 로고" v-bind="imgAttributes" /></p>
+</template>
+<style></style>
