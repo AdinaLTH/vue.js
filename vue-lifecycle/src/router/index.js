ba6fefb4a19399f0feebcfc4d8cb5aa01d649dfd
taehotaeho <jackt0506@naver.com>
2026-03-04 12:03:11 +0900

    0304 3교시

--- vue-lifecycle/src/router/index.js
+++ vue-lifecycle/src/router/index.js
@@ -1,8 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/watch",
+      name: "watchTest",
+      component: () => import("../views/WatchTest.vue"),
+    },
+    {
+      path: "/hook",
+      name: "lifecycleHook",
+      component: () => import("../views/LifecycleHook.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
