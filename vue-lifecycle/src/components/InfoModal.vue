fdfa268e0dcb9414fd35224ba1a8d29fa62f994f
taehotaeho <jackt0506@naver.com>
2026-03-04 17:51:32 +0900

    0304 8교시

--- vue-lifecycle/src/components/InfoModal.vue
+++ vue-lifecycle/src/components/InfoModal.vue
@@ -1,12 +1,20 @@
 <script setup>
 import ModelLayout from "@/layout/ModelLayout.vue";
+const checkFun = () => {
+  console.log("성공적으로 처리");
+};
+const qanFun = () => {
+  console.log("문의게시판으로 연결");
+};
 </script>
 <template>
   <ModelLayout>
     <template v-slot:footer>
-      <button>예</button>
-      <button>문의하기로</button>
+      <button v-on:click="checkFun()">예</button>
+      <button v-on:click="qanFun()">문의하기로</button>
     </template>
-    <template v-slot> </template>
+    <template v-slot:default>
+      <p>관리자에게 문의하세요</p>
+    </template>
   </ModelLayout>
 </template>
--- vue-lifecycle/src/views/CommonSlot.vue
+++ vue-lifecycle/src/views/CommonSlot.vue
@@ -1,7 +1,9 @@
 <script setup>
 import FancyButton from "@/components/FancyButton.vue";
+import InfoModal from "@/components/InfoModal.vue";
 </script>
 <template>
   <FancyButton> Click Me!! </FancyButton>
   <FancyButton> Insert Btn </FancyButton>
+  <InfoModal />
 </template>
